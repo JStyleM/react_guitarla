3422004a65d47490123faada63aba96a6dad0594
Jhonatan Moncada <jhonatanmoncada@gmail.com>
2024-03-24 13:36:15 -0500

    'CommitInicial'

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+import Guitar from "./components/Guitar";
+import Header from "./components/Header";
+
+function App() {
+
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <Header />
+
+            <main className="container-xl mt-5">
+                <h2 className="text-center">Nuestra Colección</h2>
+
+                <div className="row mt-5">
+
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+
+                </div>
+            </main>
+
+            <footer className="bg-dark mt-5 py-5">
+                <div className="container-xl">
+                    <p className="text-white text-center fs-4 mt-4 m-md-0">
+                        GuitarLA - Todos los derechos Reservados
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+}
+
+export default App;
